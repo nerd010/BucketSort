e84d36983327e03b6f599190f5438cf51ab7d8a3
smallX <xieyu559@163.com>
2017-07-29 12:22:01 +0800

    add files

--- BucketSort/main.c
+++ BucketSort/main.c
@@ -8,8 +8,40 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    /*
+     5个人的成绩分别为8，5，5，3，2
+     总分为10分
+     排序：大-->小，小-->大
+     */
+    int book[11],j,i,n;
+    for (i = 0; i <= 10; i++)
+    {
+        book[i] = 0;//初使化数组为0，表示现在任何成绩都为0
+    }
+    for (i = 0; i< 5; i++)
+    {
+        printf("请输入学生的成绩:");
+        scanf("%d",&n);
+        book[n]++;
+    }
+    printf("从小到大的排序为\n");
+    for (i = 0; i <= 10; i++)
+    {
+        for (j = 1; j <= book[i]; j++)
+        {
+            printf("%d",i);
+        }
+    }
+    printf("\n从大到小的排序为\n");
+    for (i = 10; i >= 0; i--)
+    {
+        for (j = 1; j <= book[i]; j++ )
+        {
+            printf("%d",i);
+        }
+    }
+    //用来暂停程序，以便查看程序输出的内容
+    getchar();getchar();
     return 0;
 }
